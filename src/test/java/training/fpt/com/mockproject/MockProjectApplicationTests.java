38445de7c3d8e8353b699711cea16bda826e172c
Hieu2895 <hieu2895@gmail.com>
2021-12-25 22:14:20 +0700

    Update Project

---
+++ src/test/java/training/fpt/com/mockproject/MockProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package training.fpt.com.mockproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
